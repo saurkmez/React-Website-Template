acdf6befba03430fa95b5617b4758fb0622d5e0f
saurkmez <saurkmez@gmail.com>
2021-06-08 23:29:48 +0300

    coverletter fix

--- src/components/HomePage/Coverletter.js
+++ src/components/HomePage/Coverletter.js
@@ -5,17 +5,14 @@ function Coverletter() {
     <div className="coverletter">
       <p style={{ fontSize: "15px" }}>Hi, my name is</p>
       <h1 style={{ margin: "0", color: "white" }}>
-        <i style={{ color: "#ffb236" }}>Sadan Alper Urkmez</i>
+        <i style={{ color: "#ffb236" }}>Alper Urkmez</i>
       </h1>
       <h1 style={{ margin: "0", color: "white" }}>
         I build things for the web.
       </h1>
       <p style={{ fontSize: "18px" }}>
         {" "}
-        I'm a Kayseri-based software engineer who specializes in building (and
-        occasionally designing) exceptional digital experiences. Currently, I'm
-        an engineer at Envest Enerji focused on building accessible,
-        human-centered products.
+        Self developed, creative and passionate front-end developer, record collector and musician, located in Turkey. 
       </p>
     </div>
   );
